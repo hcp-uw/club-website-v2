a79b3d5c331ecddfbfc1cb50468160f47daa5dd4
Elijah Melton <elimelt@uw.edu>
2024-07-22 19:04:50 -0700

    removed console log

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-console.log('Hello, world!')
-console.log(process.env.PUBLIC_URL)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
