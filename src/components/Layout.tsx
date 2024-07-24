e4ecf0eafa4e9e30072088eab8b977393a60cf97
Rasmus <rasmus.makiniemi@gmail.com>
2024-07-23 22:04:42 -0700

    started work on front end

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,6 +8,7 @@ import {
   Link,
   HStack,
   Button,
+  transition,
 } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 import { FaGithub, FaEnvelope, FaTwitter, FaLinkedin, FaInstagram, FaChevronRight } from 'react-icons/fa'
@@ -24,10 +25,13 @@ const NavItem = ({
     <Box
       px={4}
       py={2}
+      mr={2}
       rounded='md'
+      fontWeight={500}
       _hover={{
         textDecoration: 'none',
-        bg: useColorModeValue('gray.100', 'gray.700')
+        bg: useColorModeValue('gray.100', 'gray.700'),
+        transition: "0.1s"
       }}
     >
       {children}
@@ -35,20 +39,56 @@ const NavItem = ({
   </RouterLink>
 )
 
-const FooterLink = ({ href, icon }: { href: string; icon: React.ReactElement }) => (
-  <Link href={href} isExternal>
-    <Box as="span" fontSize="xl">
-      {icon}
-    </Box>
-  </Link>
+const FooterContainer = ({
+  children,
+  title
+}: {
+  children: React.ReactNode
+  title: string
+}) => (
+  <Flex direction={'column'} mr={20}>
+    <Text fontWeight="500" fontSize="2xl" mb={2}>{title}</Text>
+    {children}
+  </Flex>
 )
+
+const FooterLink = ({ link, type, name }: { link: string; type: string, name: string }) => {
+  if (type === "general") {
+    return (
+      <RouterLink to={link}>
+        <Text fontSize="lg" mb={0.5} _hover={{color: "#7448f7", transition: "0.2s"}}>{name}</Text>
+      </RouterLink>
+    )
+  } else if (type === "follow") {
+    return (
+      <Link fontSize="lg" mb={0.5} href={link} isExternal _hover={{ color: "#7448f7", transition: "0.05s", textDecoration: "none"}}>
+        {name}
+      </Link>
+    )
+  } else {
+    return (
+      <Link fontSize="lg" mb={0.5} href={"mailto:" + link} isExternal _hover={{ color: "#7448f7", transition: "0.05s", textDecoration: "none"}}>
+        {name}
+      </Link>
+    )
+  }
+
+}
+
+// const FooterLink = ({ href, icon }: { href: string; icon: React.ReactElement }) => (
+//   <Link href={href} isExternal>
+//     <Box as="span" fontSize="xl">
+//       {icon}
+//     </Box>
+//   </Link>
+// )
 
 const MotionBox = motion(Box)
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children
 }) => {
-  const [isExpanded, setIsExpanded] = useState(false)
+  // const [isExpanded, setIsExpanded] = useState(false)
   const bgColor = useColorModeValue('gray.50', 'gray.900')
   const navBgColor = useColorModeValue('white', 'gray.800')
   const footerBgColor = useColorModeValue('gray.50', 'gray.900')
@@ -58,19 +98,18 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
     </span>
   )
 
-  const toggleExpand = () => setIsExpanded(!isExpanded)
+  // const toggleExpand = () => setIsExpanded(!isExpanded)
 
   return (
     <Box minH='100vh' bg={bgColor} display="flex" flexDirection="column">
       <Box
         bg={navBgColor}
-        boxShadow='sm'
-        position='fixed'
+        // boxShadow='sm'
         width='full'
         zIndex={10}
       >
-        <Container maxW='container.xl'>
-          <Flex h={16} alignItems='center' justifyContent='space-between'>
+        <Container maxW='6xl'>
+          <Flex h={24} alignItems='center' justifyContent='space-between'>
             <Flex alignItems='center'>
               <RouterLink to='/'>
                 <Logo />
@@ -78,20 +117,33 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
               <NavItem to='/members'>Members</NavItem>
               <NavItem to='/teams'>Teams</NavItem>
               <NavItem to='/events'>Events</NavItem>
-              <NavItem to='/about'>About</NavItem>
+              {/* <NavItem to='/about'>About</NavItem> */}
               <NavItem to='/join'>Join</NavItem>
             </Flex>
           </Flex>
         </Container>
       </Box>
-      <Container maxW='container.xl' pt={20} flex={1}>
+      <Container maxW='5xl' flex={1}>
         {children}
       </Container>
-      <Box as="footer" bg={footerBgColor} py={6} mt={8}>
-        <Container maxW='container.xl'>
-          <Flex justifyContent="space-between" alignItems="center" flexWrap="wrap">
-            <Flex alignItems="center">
-              <Button
+      <Box as="footer" bg={footerBgColor}>
+        <Container maxW='5xl'>
+          <Flex py="10" justifyContent="space-between" alignItems="center" flexWrap="wrap">
+            <Flex justifyContent={'center'}>
+              <FooterContainer title='General'>
+                <FooterLink link='/members' name='Members' type='general'/>
+                <FooterLink link='/teams' name='Teams' type='general'/>
+                <FooterLink link='/events' name='Events' type='general'/>
+              </FooterContainer>
+              <FooterContainer title='Follow Us'>
+                <FooterLink link='https://github.com/hcp-uw' name='GitHub' type='follow'/>
+                <FooterLink link='https://www.instagram.com/hcp.uw' name='Instagram' type='follow'/>
+                <FooterLink link='https://www.linkedin.com/company/hcp-uw/mycompany/' name='LinkedIn' type='follow'/>
+              </FooterContainer>
+              <FooterContainer title='Contact Us'>
+                <FooterLink link='meow@gmail.com' name='meow@gmail.com' type='contact'/>
+              </FooterContainer>
+              {/* <Button
                 onClick={toggleExpand}
                 variant="link"
                 fontWeight="bold"
@@ -100,8 +152,8 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
                 transition="transform 0.3s"
               >
                 Connect with us
-              </Button>
-              <AnimatePresence>
+              </Button> */}
+              {/* <AnimatePresence>
                 {isExpanded && (
                   <MotionBox
                     initial={{ opacity: 0, width: 0 }}
@@ -120,7 +172,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
                     </HStack>
                   </MotionBox>
                 )}
-              </AnimatePresence>
+              </AnimatePresence> */}
             </Flex>
             <Text>&copy; {new Date().getFullYear()} Husky Coding Project. All rights reserved.</Text>
           </Flex>
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,102 +1,129 @@
 import { Layout } from "../components/Layout"
-import { Box, Flex, Text, Grid, GridItem } from "@chakra-ui/react"
+import { Box, Flex, Text, Grid, GridItem, useColorModeValue, Heading } from "@chakra-ui/react"
 import { AnimatedTerminal } from "../components/AnimatedTerminal" // Make sure to import the AnimatedTerminal component
 
+
+
 export const AboutPage: React.FC = () => {
+  const bgColor = useColorModeValue('gray.50', 'gray.900')
+  const navBgColor = useColorModeValue('white', 'gray.800')
+  const footerBgColor = useColorModeValue('gray.50', 'gray.900')
+
   return (
     <Layout>
-      <Grid templateColumns={["1fr", "1fr", "2fr 1fr"]} gap={6}>
-        <GridItem>
+      <Flex direction={'column'} justifyContent="space-between" alignItems='center'>
+        <Box bgColor={'white'} h={"60vh"} w='100vw'>
+          <Heading mt="40" textAlign={'center'} fontSize={"7xl"} fontWeight={600}>
+            Husky Coding Project
+          </Heading>
+          <Text mt="10" textAlign={'center'} fontSize={"3xl"} fontWeight={500}>
+            We're the best club there is.
+          </Text>
+        </Box>
+        <Grid mt="20" templateColumns={["1fr", "1fr", "2fr 1fr"]} gap={6} w='5xl'>
+          <GridItem>
           <Text
-            fontSize={["3xl", "4xl", "5xl", "6xl"]}
-            fontWeight="semibold"
-            marginTop="1em"
-          >
-            The Problem
-          </Text>
-          <Text fontSize={["md", "xl", "2xl"]} marginTop="1em">
-            The majority of projects offered by CS coursework are
-            solo or in pairs, so future programmers miss out on the
-            invaluable experience of working in larger teams.
-            Additionally, the leading motivation to do CS class
-            projects are for grades, rather than for the learning
-            experience.
-          </Text>
-
-          <Flex
-            direction="column"
-            width={"100%"}
-            marginTop={"3em"}
-          >
+              fontSize={["3xl", "4xl", "5xl", "6xl"]}
+              fontWeight="semibold"
+            >
+              The Problem IS THAT THERE IS TOO MUCH TEXT HERE. NEED TO REWRITE THIS ALL
+            </Text>
             <Text
               fontSize={["3xl", "4xl", "5xl", "6xl"]}
               fontWeight="semibold"
             >
-              The Why
+              ALSO WHERE SHOULD WE PUT THE CODING SNIPPET? I REALLY LIKE BUT IT'S JUST SO OUT OF PLACE ATM
             </Text>
-
+            <Text
+              fontSize={["3xl", "4xl", "5xl", "6xl"]}
+              fontWeight="semibold"
+            >
+              The Problem
+            </Text>
             <Text fontSize={["md", "xl", "2xl"]} marginTop="1em">
-              Getting internships is the best ways to gain real world
-              experience on the job. However, it is also quite
-              difficult to obtain without having some prior
+              The majority of projects offered by CS coursework are
+              solo or in pairs, so future programmers miss out on the
+              invaluable experience of working in larger teams.
+              Additionally, the leading motivation to do CS class
+              projects are for grades, rather than for the learning
               experience.
             </Text>
-            <Text fontSize={["md", "xl", "2xl"]} marginTop="1em">
-              This circular logic plagues every generation of
-              programmers.
-            </Text>
-          </Flex>
 
-          <Flex
-            direction="column"
-            width={"100%"}
-            marginTop={"3em"}
-          >
-            <Text
-              fontSize={["3xl", "4xl", "5xl", "6xl"]}
-              fontWeight="semibold"
+            <Flex
+              direction="column"
+              width={"100%"}
+              marginTop={"3em"}
             >
-              Our Mission
-            </Text>
-            <Box marginBottom={"3em"}>
               <Text
-                fontSize={["md", "xl", "2xl"]}
-                display="inline"
-                marginTop="1em"
+                fontSize={["3xl", "4xl", "5xl", "6xl"]}
+                fontWeight="semibold"
               >
-                To create a tech internship-like environment that
-                promotes the growth and development of our club
-                members. We provide team-based programming project
-                experience and encourage
+                The Why
               </Text>
-              <Text
-                fontSize={["md", "xl", "2xl"]}
-                display="inline"
-                color="brand.pink"
-              >
-                {} peer-to-peer learning
-              </Text>
-              <Text fontSize={["md", "xl", "2xl"]} display="inline">
-                {} to provide the next generation of programmers &
-                designers with the tools and experience to
-              </Text>
-              <Text
-                fontSize={["md", "xl", "2xl"]}
-                display="inline"
-                color="brand.pink"
-              >
-                {} succeed in future tech careers.
-              </Text>
-            </Box>
-          </Flex>
-        </GridItem>
 
-        <GridItem>
-          <Box position="sticky" top="20px" >
-            <AnimatedTerminal />
-          </Box>
-        </GridItem>
-      </Grid>
+              <Text fontSize={["md", "xl", "2xl"]} marginTop="1em">
+                Getting internships is the best ways to gain real world
+                experience on the job. However, it is also quite
+                difficult to obtain without having some prior
+                experience.
+              </Text>
+              <Text fontSize={["md", "xl", "2xl"]} marginTop="1em">
+                This circular logic plagues every generation of
+                programmers.
+              </Text>
+            </Flex>
+
+            <Flex
+              direction="column"
+              width={"100%"}
+              marginTop={"3em"}
+            >
+              <Text
+                fontSize={["3xl", "4xl", "5xl", "6xl"]}
+                fontWeight="semibold"
+              >
+                Our Mission
+              </Text>
+              <Box marginBottom={"3em"}>
+                <Text
+                  fontSize={["md", "xl", "2xl"]}
+                  display="inline"
+                  marginTop="1em"
+                >
+                  To create a tech internship-like environment that
+                  promotes the growth and development of our club
+                  members. We provide team-based programming project
+                  experience and encourage
+                </Text>
+                <Text
+                  fontSize={["md", "xl", "2xl"]}
+                  display="inline"
+                  color="brand.pink"
+                >
+                  {} peer-to-peer learning
+                </Text>
+                <Text fontSize={["md", "xl", "2xl"]} display="inline">
+                  {} to provide the next generation of programmers &
+                  designers with the tools and experience to
+                </Text>
+                <Text
+                  fontSize={["md", "xl", "2xl"]}
+                  display="inline"
+                  color="brand.pink"
+                >
+                  {} succeed in future tech careers.
+                </Text>
+              </Box>
+            </Flex>
+          </GridItem>
+
+          <GridItem>
+            <Box position="sticky" top="20px" >
+              <AnimatedTerminal />
+            </Box>
+          </GridItem>
+        </Grid>
+      </Flex>
     </Layout>
   );
 }
