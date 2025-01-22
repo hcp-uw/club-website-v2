72bc92b0013dc9c394c562a5eeacd794ab7badf1
Elijah Melton <elimelt@uw.edu>
2025-01-21 18:50:24 -0800

    idk if this fixes anything

--- src/service/client.ts
+++ src/service/client.ts
@@ -5,6 +5,10 @@ import { Database } from '../database.types'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and key must be provided. I\'m not sure why they aren\'t right now...')
+}
+
 const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
 export { supabase }
