a407e6ed3c256865e1a7d5b91937c5f90333cc18
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-09-18 22:42:04 -0700

    Create Window component

---
+++ src/components/Window.tsx
@@ -0,0 +1,76 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+export default function Window({
+  title,
+  children,
+  ...props
+}: {
+  title?: string;
+  children?: ReactNode;
+  [key: string]: any;
+}) {
+  return (
+    <Box
+      borderWidth="2px"
+      borderColor="black"
+      borderRadius="10px"
+      overflow="hidden"
+      bg="white"
+      {...props}
+    >
+      {/* Window Header */}
+      <Flex
+        align="center"
+        justify="space-between"
+        pl="16px"
+        pr="12px"
+        py="8px"
+        bg="white"
+        borderBottom="2px"
+        borderColor="black"
+      >
+        <Text
+          fontSize="12px"
+          fontFamily="Space Mono, monospace"
+          color="palette.darkPurple"
+          casing="uppercase"
+        >
+          {title}
+        </Text>
+        <Flex gap="14px" align="center">
+          {/* Minimize*/}
+          <Box w="15px" h="1.5px" bg="black" />
+
+          {/* Maximize */}
+          <Box w="14px" h="13px" border="2px" borderColor="black" />
+
+          {/* Close */}
+          <Box w="14px" h="13px" position="relative">
+            <Box
+              position="absolute"
+              top="50%"
+              left="50%"
+              w="17px"
+              h="1.5px"
+              bg="black"
+              transform="translate(-50%, -50%) rotate(42deg)"
+            />
+            <Box
+              position="absolute"
+              top="50%"
+              left="50%"
+              w="17px"
+              h="1.5px"
+              bg="black"
+              transform="translate(-50%, -50%) rotate(-42deg)"
+            />
+          </Box>
+        </Flex>
+      </Flex>
+
+      {/* Window Body */}
+      {children}
+    </Box>
+  );
+}
